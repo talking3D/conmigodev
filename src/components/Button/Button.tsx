233364f13d13505b7c4fd8c43d40956be13bcd2e
Krzysztof Skwarski <talking3d@gmail.com>
2023-11-24 18:51:21 +0100

    refactor: :adhesive_bandage: Add children props to the button component
    Button text is now added via its children (as text)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,6 +3,10 @@
 import React from 'react';
 import { ButtonStyled } from './Button.styles';
 
-export const Button: React.FC = () => {
-    return <ButtonStyled>Read case study</ButtonStyled>;
+export type ButtonPropsType = {
+    children: React.ReactNode;
+};
+
+export const Button: React.FC<ButtonPropsType> = ({ children }) => {
+    return <ButtonStyled>{children}</ButtonStyled>;
 };
