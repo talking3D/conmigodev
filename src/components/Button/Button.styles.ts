96ca5fc057a02ccef0f1bc6beedb15742037faff
Krzysztof Skwarski <talking3d@gmail.com>
2023-11-25 15:24:19 +0100

    feat: :dizzy: Add animated hover effect to the button component
    Chcnge button background color when hovering over

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -7,17 +7,53 @@ interface ButtonStyledProps {
 }
 
 export const ButtonStyled = styled.div<ButtonStyledProps>`
+    display: inline-block;
+    position: relative;
     border-radius: 50px;
     padding: 1.25rem 3rem;
     color: ${theme.colors.white};
     font-size: 1.25rem;
-    background-color: ${(props) =>
-        props.color === 'orange'
-            ? `${theme.colors.oranger}`
-            : `${theme.colors.bluish}`};
     font-family: ${inter.style.fontFamily};
     font-weight: 700;
     ${viewport.media.md} {
         font-size: 2rem;
+    }
+    overflow: hidden;
+    z-index: 1;
+
+    &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 0%;
+        height: 0%;
+        border-radius: 50px;
+        background-color: ${(props) =>
+            props.color === 'orange'
+                ? `${theme.colors.bluish}`
+                : `${theme.colors.orangeText}`};
+        z-index: -1;
+        transition: all 0.3s ease-in-out;
+    }
+    &:after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        border-radius: 50px;
+        color: ${theme.colors.white};
+        background-color: ${(props) =>
+            props.color === 'orange'
+                ? `${theme.colors.orangeText}`
+                : `${theme.colors.bluish}`};
+        z-index: -2;
+        transition: all 0.3s ease-in-out;
+    }
+    &:hover:before {
+        width: 100%;
+        height: 100%;
     }
 `;
